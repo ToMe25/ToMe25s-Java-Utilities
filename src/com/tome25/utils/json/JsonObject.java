1d3316e0183a55301a9eb94ec128e805c04d835d
ToMe25 <38815969+ToMe25@users.noreply.github.com>
2020-02-28 11:15:29 +0100

    remove deprecated parsing methods
    move parsing to its own class.

---
+++ src/com/tome25/utils/json/JsonObject.java
@@ -0,0 +1,111 @@
+package com.tome25.utils.json;
+
+import java.security.InvalidKeyException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class JsonObject {
+
+	private Map<String, Object> content;
+
+	public JsonObject() {
+		content = new HashMap<String, Object>();
+	}
+
+	public JsonObject(String key, Object value) {
+		content = new HashMap<String, Object>();
+		content.put(key, value);
+	}
+
+	/**
+	 * Adds the Value with the given key if there are no Object with this Key.
+	 * 
+	 * @param key   the Key.
+	 * @param value the Value.
+	 * @throws InvalidKeyException when there Allready an Object with this Key.
+	 */
+	public void add(String key, Object value) throws InvalidKeyException {
+		if (!content.containsKey(key)) {
+			content.put(key, value);
+		} else {
+			throw new InvalidKeyException("Key \"" + key + "\" Allready exists!");
+		}
+	}
+
+	public void put(String key, Object value) {
+		content.put(key, value);
+	}
+
+	public void remove(String key) {
+		content.remove(key);
+	}
+
+	public Object get(String key) {
+		return content.get(key);
+	}
+
+	public Set<String> getKeySet() {
+		return content.keySet();
+	}
+
+	public boolean contains(String key) {
+		return content.containsKey(key);
+	}
+
+	/**
+	 * 
+	 * @return the size of this Object.
+	 */
+	public int size() {
+		return content.size();
+	}
+
+	/**
+	 * Returns a String representation of this element.
+	 */
+	@Override
+	public String toString() {
+		String ret = "{";
+		for (String s : content.keySet()) {
+			ret += "\"";
+			ret += s;
+			ret += "\":";
+			Object obj = content.get(s);
+			if (obj instanceof Boolean || obj instanceof Integer) {
+				ret += obj;
+				ret += ",";
+			} else {
+				ret += "\"";
+				ret += obj.toString().replaceAll("\\\\", "\\\\\\\\").replaceAll("\\\"", "\\\\\"");
+				ret += "\",";
+			}
+		}
+		if (ret.endsWith(",")) {
+			ret = ret.substring(0, ret.length() - 1);
+		}
+		ret += "}";
+		return ret;
+	}
+
+	/**
+	 * Returns a String representation of this element as byte Array.
+	 */
+	public byte[] toByteArray() {
+		return this.toString().getBytes();
+	}
+
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		JsonObject clone = new JsonObject();
+		for (String s : content.keySet()) {
+			try {
+				clone.add(s, content.get(s));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return clone;
+	}
+
+}
---
+++ src/com/tome25/utils/json/JsonParser.java
@@ -0,0 +1,225 @@
+package com.tome25.utils.json;
+
+import java.security.InvalidKeyException;
+import java.text.ParseException;
+
+public class JsonParser {
+
+	/**
+	 * parses the given String to a JsonObject. faster but can only parse String and
+	 * Integer Objects inside of JSON's no booleans no Other Json's etc. Not as
+	 * reliable as the slower one.
+	 * 
+	 * WARNING: This method is not safe, over the time i worked on it before adding
+	 * it to this library there were multiple character that could make it crash or
+	 * break out of String values.
+	 * 
+	 * @param s the String to parse
+	 * @return the Json Object parsed from String s
+	 * @throws ParseException
+	 */
+	public static JsonObject parseStringFast(String str) throws ParseException {
+		JsonObject json = new JsonObject();
+		if (str.contains("{") && str.contains("}")) {
+			str = str.substring(str.indexOf("{") + 1, str.lastIndexOf("}"));
+		} else if (str.contains("{")) {
+			throw new ParseException(str, str.length());
+		} else {
+			throw new ParseException(str, 0);
+		}
+		for (String s : str.split(",")) {
+			if (s.contains(":")) {
+				String[] pair = s.split(":");
+				String key = pair[0];
+				String value = pair[1];
+				key = key.replaceAll("\"", "");
+				if (value.contains("\"")) {
+					json.put(key, value.replaceAll("\"", ""));
+				} else {
+					json.put(key, Integer.parseInt(value));
+				}
+			}
+		}
+		return json;
+	}
+
+	/**
+	 * parses the given String to a JsonObject.
+	 * 
+	 * WARNING: This method is not safe, over the time i worked on it before adding
+	 * it to this library there were multiple character that could make it crash or
+	 * break out of String values.
+	 * 
+	 * @param s the String to parse
+	 * @return the Json Object parsed from String s
+	 * @throws InvalidKeyException
+	 * @throws ParseException
+	 */
+	public static JsonObject parseString(String s) throws InvalidKeyException, ParseException {
+		JsonObject json = null;
+		boolean buildString = false;
+		boolean buildJson = false;
+		boolean buildOther = false;
+		boolean escaped = false;
+		String key = null;
+		String buffer = null;
+		short layer = 0;
+		short offset = 0;
+		for (char c : s.toCharArray()) {
+			switch (c) {
+			case '{':
+				if (json == null) {
+					json = new JsonObject();
+				} else if (buildString) {
+					buffer += c;
+				} else if (buildJson) {
+					buffer += c;
+					layer++;
+				} else {
+					buffer = "{";
+					buildJson = true;
+					layer++;
+				}
+				break;
+
+			case '\\':
+				if (escaped) {
+					buffer += c;
+					escaped = false;
+				} else if (buildJson) {
+					buffer += c;
+					escaped = true;
+				} else {
+					escaped = true;
+				}
+				break;
+
+			case '"':
+				if (escaped) {
+					buffer += c;
+					escaped = false;
+				} else if (buildJson) {
+					buffer += c;
+					buildString = !buildString;
+				} else if (buildString) {
+					if (key == null) {
+						key = buffer;
+						buffer = null;
+					} else {
+						json.add(key, buffer);
+						key = null;
+						buffer = null;
+					}
+					buildString = false;
+				} else {
+					buffer = "";
+					buildString = true;
+				}
+				break;
+
+			case ':':
+				if (buildString || buildJson) {
+					buffer += c;
+				}
+				break;
+
+			case ',':
+				if (buildString || buildJson) {
+					buffer += c;
+				} else if (buildOther) {
+					try {
+						int i = Integer.parseInt(buffer);
+						json.add(key, i);
+						key = null;
+						buffer = null;
+						buildOther = false;
+					} catch (Exception e) {
+						if (buffer.equalsIgnoreCase("true")) {
+							if (key == null) {
+								throw new ParseException("Key Missing!", offset);
+							}
+							json.add(key, true);
+							key = null;
+							buffer = null;
+							buildOther = false;
+						} else if (buffer.equalsIgnoreCase("false")) {
+							if (key == null) {
+								throw new ParseException("Key Missing!", offset);
+							}
+							json.add(key, false);
+							key = null;
+							buffer = null;
+							buildOther = false;
+						} else {
+							throw new ParseException("type for value \"" + buffer + "\" Unknown!", offset);
+						}
+					}
+				}
+				break;
+
+			case '}':
+				if (buildString) {
+					buffer += c;
+				} else if (buildJson) {
+					buffer += c;
+					if (layer > 0) {
+						layer--;
+					}
+					if (layer <= 0) {
+						JsonObject subjson = parseString(buffer);
+						if (key == null) {
+							throw new ParseException("Key Missing!", offset);
+						}
+						json.add(key, subjson);
+						key = null;
+						buffer = null;
+						buildJson = false;
+					}
+				} else if (buildOther) {
+					try {
+						int i = Integer.parseInt(buffer);
+						json.add(key, i);
+						key = null;
+						buffer = null;
+						buildOther = false;
+					} catch (Exception e) {
+						if (buffer.equalsIgnoreCase("true")) {
+							if (key == null) {
+								throw new ParseException("Key Missing!", offset);
+							}
+							json.add(key, true);
+							key = null;
+							buffer = null;
+							buildOther = false;
+						} else if (buffer.equalsIgnoreCase("false")) {
+							if (key == null) {
+								throw new ParseException("Key Missing!", offset);
+							}
+							json.add(key, false);
+							key = null;
+							buffer = null;
+							buildOther = false;
+						} else {
+							throw new ParseException("type for value \"" + buffer + "\" Unknown!", offset);
+						}
+					}
+				} else {
+					return json;
+				}
+				break;
+
+			default:
+				if (buffer == null) {
+					buffer = "" + c;
+					buildOther = true;
+				} else if (buildString || buildJson || buildOther) {
+					buffer += c;
+				}
+				break;
+			}
+			offset++;
+		}
+		return json;
+	}
+
+}

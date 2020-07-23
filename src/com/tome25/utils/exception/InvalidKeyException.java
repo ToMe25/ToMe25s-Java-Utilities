e79d2c93ecd0752b04d9a901b5244ad544c23b44
ToMe25 <38815969+ToMe25@users.noreply.github.com>
2020-07-23 10:51:00 +0200

    Add a bit exception javadoc

--- src/com/tome25/utils/exception/InvalidKeyException.java
+++ src/com/tome25/utils/exception/InvalidKeyException.java
@@ -1,7 +1,7 @@
 package com.tome25.utils.exception;
 
 /**
- * A simple Exception for Invalid keys.
+ * A simple {@link Exception} for invalid keys.
  * 
  * @author ToMe25
  */
@@ -10,9 +10,17 @@ public class InvalidKeyException extends RuntimeException {
 	private static final long serialVersionUID = -5981734076788525548L;
 	private String message;
 
+	/**
+	 * Creates a new empty InvalidKeyException.
+	 */
 	public InvalidKeyException() {
 	}
 
+	/**
+	 * Creates a new InvalidKeyException with the given message.
+	 * 
+	 * @param msg the message for this InvalidKeyException.
+	 */
 	public InvalidKeyException(String msg) {
 		message = msg;
 	}
--- src/com/tome25/utils/exception/InvalidTypeException.java
+++ src/com/tome25/utils/exception/InvalidTypeException.java
@@ -1,7 +1,7 @@
 package com.tome25.utils.exception;
 
 /**
- * A simple Exception for Invalid types.
+ * A simple {@link Exception} for invalid types.
  * 
  * @author ToMe25
  */
@@ -10,15 +10,29 @@ public class InvalidTypeException extends RuntimeException {
 	private static final long serialVersionUID = -5981734076788525548L;
 	private String message;
 
+	/**
+	 * Creates a new InvalidTypeException.
+	 */
 	public InvalidTypeException() {
 	}
 
+	/**
+	 * Creates a new InvalidTypeException with the given message.
+	 * 
+	 * @param msg the message for this InvalidTypeException.
+	 */
 	public InvalidTypeException(String msg) {
 		message = msg;
 	}
 
+	/**
+	 * Creates a new InvalidTypeException with a message about the given expected and received type.
+	 * 
+	 * @param expectedType the expected type.
+	 * @param receivedType the received type.
+	 */
 	public InvalidTypeException(String expectedType, String receivedType) {
-		message = String.format("Received invalid type %s, expected %s.", receivedType, expectedType);
+		this(String.format("Received invalid type %s, expected %s.", receivedType, expectedType));
 	}
 
 	@Override
